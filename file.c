7f41450dfec4ac066fab1ed3bf3a7bb77efffcae
Pi291 <w.tommi100@gmail.com>
2025-10-12 16:52:17 +0400

    /

--- file.c
+++ file.c
@@ -1,26 +1,13 @@
 #include <stdio.h>
-
-#define IN 1 
-#define OUT 0
-
-int main () 
+int main()
 {
-  int c, nl, nw, nc, state;
-  state = OUT;
-  nl = nw = nc = 0;
-  while ((c = getchar()) != EOF) 
-  {
-    ++nc;
-    if (c == '\n')
-      ++nl;
-    if (c == ' ' || c == '\n' || c == '\t')
-      state = OUT;
-    else if (state == OUT) 
-    {
-      state = IN;
-      ++nw;
+  int letters;
+  while((letters = getchar()) != EOF) {
+    if (letters == ' ' || letters == '\t' || letters == '\n'){
+     putchar('\n');
+    } else {
+    putchar(letters);
     }
   }
-  printf("%d %d %d\n", nl, nw, nc);
   return 0;
 }
---
+++ print.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+ int main() {
+   int word;
+   while ((c = getchar()) != EOF) {
+     printf("%d/n", word);
+     RETURN 0;
+   }
+ }
